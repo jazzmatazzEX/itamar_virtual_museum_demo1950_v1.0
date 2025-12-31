9992fe2bf4767352600326be94f7bee54bc3d60c
jazzmatazzEX <lucasf.candido97@gmail.com>
2025-12-31 10:42:13 +0100

    add vite.config file

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  optimizeDeps: { exclude: ['lucide-react'], },
+  base: mode === 'production'
+    ? '/itamar_virtual_museum_demo1950_v1.0/'
+    : '',
+}));
+
